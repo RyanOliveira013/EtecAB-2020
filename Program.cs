0efdc57d3e80bb231f1d33ce9597d1bc69ac4244
Ryan Oliveira013 <RyanOliveira.2003.c@gmail.com>
2020-09-08 22:20:57 -0300

    configuraçao inicial do programa

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EtecAB_2020
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Etec adolpho Berezin");
+        }
+    }
+}
